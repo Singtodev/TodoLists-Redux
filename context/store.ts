c2a89db193bd4f3901e7e17619e2a3ce401ee72f
Singtodev <138797225+Unkeloc4@users.noreply.github.com>
2023-09-03 19:19:09 +0700

    Update store.ts

--- context/store.ts
+++ context/store.ts
@@ -5,14 +5,18 @@ import rootReducer from './reducers';
 let store: any;
 
 
-if(typeof window !== "undefined"){
-    store = createStore(rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );    
-}else{
-    store = createStore(
-        rootReducer
-    )
-}
+// if(typeof window !== "undefined"){
+//     store = createStore(rootReducer,
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     );    
+// }else{
+//     store = createStore(
+//         rootReducer
+//     )
+// }
+
+store = createStore(
+    rootReducer
+)
 
 export default store
