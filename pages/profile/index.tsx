d41bdca4f3bd15f75b8fec426ac190a342a48896
Singtodev <138797225+Unkeloc4@users.noreply.github.com>
2023-09-05 23:40:27 +0700

    update

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
             <div className="flex flex-col gap-4">
                 <Link href="/" className="text-white hover:border px-5 hover:border-white rounded-md"> Go Home</Link>
                 <Link href="/profile" className="text-white border px-5 border-white rounded-md"> Go Profile</Link>
-                <Link href="/fetching" className="text-white px-5  rounded-md"> Go Fetching</Link>
+                <Link href="/product" className="text-white px-5  rounded-md"> Go Fetching Product</Link>
                 <div  
                 onClick={()=> handleLogout()}
                 className="text-white px-5"> Go logout </div>
